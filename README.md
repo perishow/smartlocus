6b0bebefcb8c39f3227de6e2dc87917a9b1ebe5a
perishow <peridelimamacedo@gmail.com>
2026-06-09 17:15:01 -0300

    (): Iniciei o README.md e estou testando o comando de persistencia do banco

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# smartlocus
+# Smartlocus
+
+Smartlocus é uma solução de gerenciamento de almoxarifado, implementada como projeto final da cadeira de "Projeto de Engenharia da Computação".
+
+## Requisitos
+
+- Docker Engine
+- Rust + Cargo: pode ser instalado como instruído no site oficial: <https://rust-lang.org/tools/install/>
+
+## O sistema funciona em 3 partes
+
+- Um banco de dados MariaDB
+- Um backend em Rust
+- Um frontend em alguma coisa que ainda vai ser implementado
+
+### Banco de dados
+
+O banco de dados MariaDB é instanciado automaticamente ao iniciar o backend e já vem com alguns dados instanciados porém, qualquer dado inserido posteriormente ao banco não é persistente e será deletado caso o banco seja reiniciado.
+Para garantir a persistência dos dados novos, o arquivo "database/init.sql" deve ser substituído por um novo dump do banco. Esse dump pode ser gerado utilizando o comando:
+
+docker compose exec mariadb mariadb-dump -u root -psenha smartlocus > dump_2026-06-09_17-03-58.sql
---
+++ database/dump_2026-06-09_17-03-58.sql
@@ -0,0 +1,168 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-12.3.2-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: smartlocus
+-- ------------------------------------------------------
+-- Server version	12.3.2-MariaDB-ubu2404
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Table structure for table `Itens`
+--
+
+DROP TABLE IF EXISTS `Itens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Itens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `categoria` varchar(100) NOT NULL,
+  `quantidade_atual` int(11) DEFAULT 0,
+  `quantidade_minima` int(11) DEFAULT 0,
+  `localizacao` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Itens`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `Itens` WRITE;
+/*!40000 ALTER TABLE `Itens` DISABLE KEYS */;
+INSERT INTO `Itens` VALUES
+(1,'Marcador/Piloto para Quadro Branco','Material de Escritório',10,2,'Prateleira A1'),
+(2,'Vassoura','Material de Limpeza',9,1,'Armário de Limpeza');
+/*!40000 ALTER TABLE `Itens` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+
+--
+-- Table structure for table `Movimentacoes_Estoque`
+--
+
+DROP TABLE IF EXISTS `Movimentacoes_Estoque`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Movimentacoes_Estoque` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) NOT NULL,
+  `tipo` enum('Entrada','Saída') NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `data_movimentacao` datetime NOT NULL,
+  `observacao` text DEFAULT NULL,
+  `responsavel_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item_id` (`item_id`),
+  KEY `responsavel_id` (`responsavel_id`),
+  CONSTRAINT `1` FOREIGN KEY (`item_id`) REFERENCES `Itens` (`id`),
+  CONSTRAINT `2` FOREIGN KEY (`responsavel_id`) REFERENCES `Usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Movimentacoes_Estoque`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `Movimentacoes_Estoque` WRITE;
+/*!40000 ALTER TABLE `Movimentacoes_Estoque` DISABLE KEYS */;
+INSERT INTO `Movimentacoes_Estoque` VALUES
+(1,1,'Entrada',10,'2026-05-16 19:18:42','Entrada inicial de lote de pilotos para quadro branco',1),
+(2,2,'Entrada',3,'2026-05-16 19:21:41','Entrada inicial de vassouras',1),
+(4,2,'Entrada',3,'2025-03-20 14:30:00','Adição realizada utilizando a rota',2),
+(5,2,'Entrada',2,'2025-03-20 14:30:00','Adição realizada utilizando a rota',2),
+(6,2,'Saída',2,'2025-03-20 14:30:00','Adição realizada utilizando a rota',2);
+/*!40000 ALTER TABLE `Movimentacoes_Estoque` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+
+--
+-- Table structure for table `Solicitacoes_Materiais`
+--
+
+DROP TABLE IF EXISTS `Solicitacoes_Materiais`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Solicitacoes_Materiais` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) NOT NULL,
+  `solicitante_id` int(11) NOT NULL,
+  `quantidade_solicitada` int(11) NOT NULL,
+  `status` enum('Pendente','Aprovada','Rejeitada') DEFAULT 'Pendente',
+  `data_solicitacao` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item_id` (`item_id`),
+  KEY `solicitante_id` (`solicitante_id`),
+  CONSTRAINT `1` FOREIGN KEY (`item_id`) REFERENCES `Itens` (`id`),
+  CONSTRAINT `2` FOREIGN KEY (`solicitante_id`) REFERENCES `Usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Solicitacoes_Materiais`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `Solicitacoes_Materiais` WRITE;
+/*!40000 ALTER TABLE `Solicitacoes_Materiais` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Solicitacoes_Materiais` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+
+--
+-- Table structure for table `Usuarios`
+--
+
+DROP TABLE IF EXISTS `Usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `perfil` enum('Consultor','Operador') NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Usuarios`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `Usuarios` WRITE;
+/*!40000 ALTER TABLE `Usuarios` DISABLE KEYS */;
+INSERT INTO `Usuarios` VALUES
+(2,'Leozinho Ruindade Pura','leozinho244@gmail.com','senha_super_segura123','Operador');
+/*!40000 ALTER TABLE `Usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+
+-- Dump completed on 2026-06-09 20:05:18
